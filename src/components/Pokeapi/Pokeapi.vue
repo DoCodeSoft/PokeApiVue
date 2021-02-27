c5e1ea8bfc5d5136bfbd7d8db620190855ddb33b
aleruizj <alex0310@live.com.mx>
2021-02-27 14:21:22 -0600

    se implementa promiseall y se modifica tecnicas

--- src/components/Pokeapi/Pokeapi.vue
+++ src/components/Pokeapi/Pokeapi.vue
@@ -19,20 +19,6 @@ import axios from "axios"
             this.obtenerApi()
         },
         methods: {
-            obtenerApiold() {
-                let url = 'https://pokeapi.co/api/v2/pokemon?offset=0&limit=10'
-                axios.get(url)
-                .then((result) => {
-                    let urls = []
-                    result.data.results.forEach(el => {
-                        urls.push(el.url)
-                    })
-                    this.obtenerDetalle(urls)
-                })
-                .catch((err) => {
-                    console.log(err)
-                })
-            },
             obtenerApi() {
                 let url = 'https://pokeapi.co/api/v2/pokemon?offset=0&limit=10'
                 axios.get(url)
@@ -41,57 +27,43 @@ import axios from "axios"
                     result.data.results.forEach(el => {
                         urls.push(el.url)
                     })
-                    this.obtenerDetalle(urls)
+                    
+                    this.nuevoObtener(urls)
                 })
                 .catch((err) => {
                     console.log(err)
                 })
             },
             nuevoObtener(urls) {
-                let rt = []
-                urls.forEach( url => {
-                    this.obtenerDetalle2(url)
-                })
-                
-            },
-            obtenerDetalle2(url) {
                 let respuesta = []
-                respuesta.push(axios.get(url)).then(()=>{
-                    Promise.all(respuesta).then((valores)=>{
-                    let pokemon = valores[0].data
-                    console.log(pokemon.id)
-                    })
-                })
-                
-            },
-            obtenerDetalle(urls) {
                 urls.forEach( url =>{
-                     axios.get(url)
-                    .then( result => {
-                        let pokemon = result.data
-                        let obj = {
+                    respuesta.push(axios.get(url))
+                })
+                Promise.all(respuesta).then((valores)=>{
+                    this.obtenerDetalle2(valores)
+                })
+            },
+            obtenerDetalle2(valores) {
+                valores.forEach((poketodo)=>{
+                    let pokemon = poketodo.data
+                    let obj = {
                             id: pokemon.id,
                             nombre: pokemon.name,
                             peso: pokemon.weight,
                             imgFront: pokemon.sprites.front_default,
                             imgBack: pokemon.sprites.back_default,
-                            moves: this.obtenerTecnicas(pokemon.moves)
+                            moves: this.obtenerTecnicas2(pokemon.moves)
                         }
-                        this.pokemones.push(obj)
-                        // Oredenar Pokemones
-                        this.pokemones.sort((a, b) => (a.id > b.id) ? 1 : -1)
-                    })
-                    .catch((err) => {
-                        console.log('error'+ err)
-                    })
+                    this.pokemones.push(obj)
                 })
-            },
-            obtenerTecnicas(moves) {
+                
+                
+            },obtenerTecnicas2(moves) {
                 let movesFinal = []
                 try {
                     let i = 0
                     for (i; i < 4; i++) {
-                        let id = Math.floor(Math.random() * moves.length) + 1
+                        let id = Math.floor(Math.random() * moves.length)
                         movesFinal.push(moves[id].move.name)
                     }
                 } catch (error) {
